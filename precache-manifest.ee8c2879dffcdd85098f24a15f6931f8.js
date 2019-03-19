67fa428afb64f4231ddb77f895ffe2d5fdd6ee1c
Yoon Wai Yan <waiyanyoon@gmail.com>
2019-03-19 21:44:21 +0800

    Updates

---
+++ precache-manifest.ee8c2879dffcdd85098f24a15f6931f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "329a08eccd47fb8c82f0",
+    "url": "/oblique-strategies/static/js/runtime~main.329a08ec.js"
+  },
+  {
+    "revision": "dd40e6387b4a7ae6f3b4",
+    "url": "/oblique-strategies/static/js/main.dd40e638.chunk.js"
+  },
+  {
+    "revision": "9aad51153a2a2f5424ae",
+    "url": "/oblique-strategies/static/js/1.9aad5115.chunk.js"
+  },
+  {
+    "revision": "dd40e6387b4a7ae6f3b4",
+    "url": "/oblique-strategies/static/css/main.4c90586d.chunk.css"
+  },
+  {
+    "revision": "5aaa29e9a28250e5ec2c4b6e684d6fca",
+    "url": "/oblique-strategies/index.html"
+  }
+];
