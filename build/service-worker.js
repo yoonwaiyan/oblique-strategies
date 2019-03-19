ed83873dcae9e2b82205248bf77b8a4cf8b862b7
Yoon Wai Yan <waiyanyoon@gmail.com>
2019-03-19 21:46:34 +0800

    Ignore build in master branch

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/oblique-strategies/precache-manifest.b095f0b35162d7ed4f116a5ac8acbadc.js"
+  "/oblique-strategies/precache-manifest.ee8c2879dffcdd85098f24a15f6931f8.js"
 );
 
 workbox.clientsClaim();
